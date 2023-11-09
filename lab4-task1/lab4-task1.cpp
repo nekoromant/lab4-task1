d598c219e05f0f8d863a53dfb254bda2ba350a9a
nekoromant <igorbog842@gmail.com>
2023-11-09 10:49:51 +0700

    Initial commit

---
+++ lab4-task1/lab4-task1.cpp
@@ -0,0 +1,22 @@
+﻿
+//Даны десять вещественных чисел. Найти произведение всех чётных чисел.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int sum = 0, num;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Введите число n" << i + 1 << ": ";
+		cin >> num;
+		if (num % 2 == 0) {
+			sum += num;
+		}
+	}
+	cout << "Сумма чётных чисел: " << sum;
+	return 0;
+}
+
